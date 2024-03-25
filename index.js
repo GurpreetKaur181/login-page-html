4981e53b1c61425c3504b426a155a97e4b44baa7
GurpreetKaur181 <gk9000219@gmail.com>
2024-03-25 14:25:51 +0530

    my file

---
+++ index.js
@@ -0,0 +1,8 @@
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let namefield = document.getElementById("namefield");
+let title = document.getElementById("title");
+
+signinBtn.onclick = function(){
+    namefield.style.maxHeight = "0";
+}
